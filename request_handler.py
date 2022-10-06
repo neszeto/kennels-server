a31a21f97087b06af7fa5a2e372a323981c0b1f9
Nora Szeto <neszeto@gmail.com>
2022-10-05 20:05:33 -0500

    built out response to GET, POST and DELETE

--- request_handler.py
+++ request_handler.py
@@ -1,6 +1,7 @@
 import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from views import get_all_animals
+from views import get_all_animals, get_single_animal, get_all_locations, get_single_location, get_all_employees, get_single_employee, get_all_customers, get_single_customer, create_animal, create_location, create_customer, create_employee, delete_animal, delete_customer, delete_employee, delete_location
+
 
 
 # Here's a class. It inherits from another class.
@@ -18,43 +19,114 @@ class HandleRequests(BaseHTTPRequestHandler):
     # Here's a method on the class that overrides the parent's method.
     # It handles any GET request.
     def do_GET(self):
-        """Handles GET requests to the server
-        """
-        # Set the response code to 'Ok'
         self._set_headers(200)
+        response = {}  # Default response
 
-        # Your new console.log() that outputs to the terminal
-        print(self.path)
+        # Parse the URL and capture the tuple that is returned
+        (resource, id) = self.parse_url(self.path)
 
-        # It's an if..else statement
-        if self.path == "/animals":
-            # In Python, this is a list of dictionaries
-            # In JavaScript, you would call it an array of objects
-            response = get_all_animals()
+        if resource == "animals":
+            if id is not None:
+                response = get_single_animal(id)
 
-        else:
-            response = []
+            else:
+                response = get_all_animals()
+        
+        if resource == "locations":
+            if id is not None:
+                response = get_single_location(id)
 
-        # Send a JSON formatted string as a response
+            else:
+                response = get_all_locations()
+
+        if resource == "employees": 
+            if id is not None:
+                response = get_single_employee(id)
+            
+            else:
+                response = get_all_employees()
+
+        if resource == "customers": 
+            if id is not None: 
+                response = get_single_customer(id)
+
+            else: 
+                response = get_all_customers()
+
+
         self.wfile.write(json.dumps(response).encode())
 
     # Here's a method on the class that overrides the parent's method.
     # It handles any POST request.
     def do_POST(self):
-        """Handles POST requests to the server"""
-
-        # Set response code to 'Created'
         self._set_headers(201)
-
         content_len = int(self.headers.get('content-length', 0))
         post_body = self.rfile.read(content_len)
-        response = { "payload": post_body }
-        self.wfile.write(json.dumps(response).encode()) #run on my local host, run on this port, start my server on those and run handleRequest class and do it forever
+
+        # Convert JSON string to a Python dictionary
+        post_body = json.loads(post_body) #converts string to a dictionary
+
+        # Parse the URL
+        (resource, id) = self.parse_url(self.path)
+
+        # Initialize new animal
+        new_animal = None
+
+        new_location = None
+
+        new_customer = None
+
+        new_employee = None
+
+        # Add a new animal to the list. Don't worry about
+        # the orange squiggle, you'll define the create_animal
+        # function next.
+        if resource == "animals":
+            new_animal = create_animal(post_body)
+
+            self.wfile.write(json.dumps(new_animal).encode())
+
+        if resource == "locations":
+            new_location = create_location(post_body)
+
+            self.wfile.write(json.dumps(new_location).encode())
+
+        if resource == "customers":
+            new_customer = create_customer(post_body)
+
+            self.wfile.write(json.dumps(new_customer).encode())
+
+        if resource == "employees":
+            new_employee = create_employee(post_body)
+
+            self.wfile.write(json.dumps(new_employee).encode())
 
     # A method that handles any PUT request.
     def do_PUT(self):
         """Handles PUT requests to the server"""
         self.do_PUT()
+
+    def do_DELETE(self):
+        # Set a 204 response code
+        self._set_headers(204)
+
+        # Parse the URL
+        (resource, id) = self.parse_url(self.path)
+
+        # Delete a single animal from the list
+        if resource == "animals":
+            delete_animal(id)
+        
+        if resource == "customers":
+            delete_customer(id)
+
+        if resource == "employees":
+            delete_employee(id)
+
+        if resource == "locations":
+            delete_location(id)
+        # Encode the new animal and send in response
+        self.wfile.write("".encode())
 
     def _set_headers(self, status):
         # Notice this Docstring also includes information about the arguments passed to the function
@@ -79,6 +151,27 @@ class HandleRequests(BaseHTTPRequestHandler):
         self.send_header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept')
         self.end_headers()
 
+    def parse_url(self, path):
+        # Just like splitting a string in JavaScript. If the
+        # path is "/animals/1", the resulting list will
+        # have "" at index 0, "animals" at index 1, and "1"
+        # at index 2.
+        path_params = path.split("/")
+        resource = path_params[1]
+        id = None
+
+        # Try to get the item at index 2
+        try:
+            # Convert the string "1" to the integer 1
+            # This is the new parseInt()
+            id = int(path_params[2])
+        except IndexError:
+            pass  # No route parameter exists: /animals
+        except ValueError:
+            pass  # Request had trailing slash: /animals/
+
+        return (resource, id)  # This is a tuple
+
 
 # This function is not inside the class. It is the starting
 # point of this application.
@@ -87,7 +180,7 @@ def main():
     """
     host = ''
     port = 8088
-    HTTPServer((host, port), HandleRequests).serve_forever() #take python and convert to json for client (take the python and dump back into string)
+    HTTPServer((host, port), HandleRequests).serve_forever() #run on my local host, run on this port, start my server on those and run handleRequest class and do it forever
 
 
 if __name__ == "__main__":
